5c6630d9cea7fdf3bdb8d2768387b4659cfa563e
Tekou Patrice <patricetekou341@gmail.com>
2026-04-10 13:31:33 +0100

    Correction de la vue login

--- gestion_stock/views.py
+++ gestion_stock/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
@@ -10,3 +13,18 @@ def gestionaire_view(request):
 
 def secretaire_view(request):
     return render(request, 'gestion/secretaire.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")  # redirige vers une page définie dans urls.py
+        else:
+            messages.error(request, "Identifiants incorrects")
+
+    return render(request, 'login.html')
