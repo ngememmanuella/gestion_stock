1f458bd459ed18d7acc6b46a6d54ffed37760ed6
NgemEmanuella <ngem.emanuella@gmail.com>
2026-04-13 20:47:25 +0200

    Finalisation du design StockPro et correction des templates

--- TP_208/settings.py
+++ TP_208/settings.py
@@ -123,4 +123,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Ajoutez ces deux lignes ici :
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
